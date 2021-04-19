a5f31e24898ed48629a4ac61c4faabe10b9e5e75
Aleksandra Fedorova <asf2003@yandex.ru>
2021-04-19 16:12:18 +0300

    Change main_test.py

--- main_test.py
+++ main_test.py
@@ -1,86 +1,92 @@
-from PyTelegramBotApi import telebot
-bot = telebot.TeleBot('1086758705:AAGsQ7j7vl9Q_25HI7Aiz0zMNyDsShcbyPw')
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
 
-operation = telebot.types.ReplyKeyboardMarkup(True, True)
-operation.row('*', '/')
-operation.row('+', '-')
-operations = ['*', '/', '+', '-']
-
-simple_text = telebot.types.ReplyKeyboardMarkup(True, True)
-simple_text.row('Калькулятор')
-user_op = ['', '']
-
-@bot.message_handler(commands=['start'])
-def start_message(message):
-  print(f'{message.chat.first_name} : {message.text}')
-  bot.send_message(message.chat.id, "Привет!")
-  print('Bot : Привет!')
-
-@bot.message_handler(commands=['help'])
-def help_message(message):
-  print(f'{message.chat.first_name} : {message.text}')
-  bot.send_message(message.chat.id, f"{message.chat.first_name}, Ничем не могу помочь!")
-  print(f'Bot : {message.chat.first_name}, Ничем не могу помочь!')
-
-@bot.message_handler(content_types=['text'])
-def text(message):
-  print(f'{message.chat.first_name} : {message.text}')
-  if message.text != 'Калькулятор':
-    bot.send_message(message.chat.id, 'Я кроме калькулятора ничего не умею.... Напиши "Калькулятор"', reply_markup = simple_text)
-    print(f'Bot : ты не прав')
-  else:
-    user_op.clear
-    m = bot.reply_to(message, 'Введите первое число')
-    bot.register_next_step_handler(m, oper)    
-    print(f'Bot : Введите первое число')
-
-def oper(message):
-  print(f'{message.chat.first_name} : {message.text}')
-  try: 
-    user_op[0] = int(message.text)
-    op = bot.reply_to(message, 'Введите операцию', reply_markup = operation)
-    bot.register_next_step_handler(op, input_sec)
-    print(f'Bot : Введите операцию')
-  except ValueError:
-    i1  = bot.reply_to(message, 'Это не число. Попробуйте ввести число!')
-    bot.register_next_step_handler(i1, oper)
-    print(f'Bot : ты не прав')
-def input_sec(message):
-  print(f'{message.chat.first_name} : {message.text}')
-  if message.text in operations:
-    user_op[1] = message.text
-    m = bot.reply_to(message, 'Введите второе число')
-    bot.register_next_step_handler(m, solve)
-    print(f'Bot : Введите второе число')
-  else:
-    m = bot.reply_to(message, "Я такое не умею. Попробуйте выбрать один из предложеных вариантов")
-    bot.register_next_step_handler(m, input_sec)
-    print(f'Bot : ты не прав')
-
-def solve(message):
-  print(f'{message.chat.first_name} : {message.text}')
-  try: 
-    i2 = int(message.text)
-    if user_op[1] == '+':
-      bot.send_message(message.chat.id, user_op[0]+i2)
-      print(f'Bot : {user_op[0]+i2}')
-    elif user_op[1] == '-':
-      bot.send_message(message.chat.id, user_op[0] - i2)
-      print(f'Bot : {user_op[0]-i2}')
-    elif user_op[1] == '*':
-      bot.send_message(message.chat.id, user_op[0]*i2)
-      print(f'Bot : {user_op[0]*i2}')
+# Define a few command handlers. These usually take the two arguments update and
+# context. Error handlers also receive the raised TelegramError object in error.
+def start(update, context):
+    """Send a message when the command /start is issued."""
+    user = update.effective_user
+    if user:
+        name = user.first_name
     else:
-      if i2 == 0:
-        bot.send_message(message.chat.id, 'На ноль делить нельзя.')
-        print(f'Bot : На ноль делить нельзя.')
-      else:
-        bot.send_message(message.chat.id, user_op[0]/i2)
-        print(f'Bot : {user_op[0]/i2}')
-  except ValueError:
-    i1  = bot.reply_to(message, 'Это не число. Попробуйте ввести число!')
-    bot.register_next_step_handler(i1, solve)
-    print(f'Bot : ты не прав')
+        name = 'Анонимус'
+    update.message.reply_text('Hi, {}!'.format(name))
+    update.message.reply_text('My name is banana-ml bot. I can make your photos look like \
+                              the famous pictures! Please, choose the option: \n \
+                              - "1" Universe style; \n \
+                              - "2" Mondrian style; \n \
+                              - "3" "Starry night" style;\n\
+                              - "4" Matiss style; \n \
+                              - "5" Simpsons style; \n\
+                              - "6" Renuar style')
 
-  
-bot.polling()
+
+def help(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Help!')
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def action(update, context):
+    """Send a message when the command /help is issued."""
+    update.message.reply_text('Здесь будет экшн')
+    text_caps = ' '.join(context.args).upper()
+    context.bot.send_message(chat_id=update.effective_chat.id, text=text_caps)
+
+def echo(update, context):
+    """Echo the user message."""
+    update.message.reply_text(update.message.text)
+    
+def get_style(update, context):
+    global option
+    option = update.message.text
+    update.message.reply_text('Good! Now input coef')
+def get_coef(update, context):
+    global coef
+    coef = update.message.number
+    update.message.reply_text('Good! Now send me a pic :)')
+    
+def get_photo(update, context):
+    """Echo the user message."""
+    user = update.message.from_user
+    # get photo file
+    photo_file = update.message.photo[-1].get_file()
+    # save photo
+    photo_file.download('user_photo.jpg')
+    logger.info("Photo of %s: %s", user.first_name, 'user_photo.jpg')
+    update.message.reply_text('Nice! Got your photo, styling...')
+    
+
+
+def main():
+    """Start the bot."""
+    print('Start')
+    updater = Updater("1247859285:AAHSu4GPAOFVrpA8ZB_dJUnibLuE3UbSio4", use_context=True)
+        # Get the dispatcher to register handlers
+    dp = updater.dispatcher
+
+    # on different commands - answer in Telegram
+    dp.add_handler(CommandHandler("start", start))
+    dp.add_handler(CommandHandler("help", help))
+    dp.add_handler(CommandHandler("action", action))
+
+    # on noncommand i.e message - echo the message on Telegram
+    dp.add_handler(MessageHandler(Filters.text, get_style))
+    dp.add_handler(MessageHandler(Filters.text, get_coef))
+    dp.add_handler(MessageHandler(Filters.photo, get_photo))
+
+    # on noncommand i.e message - echo the message on Telegram
+    # log all errors
+    dp.add_error_handler(error)
+
+    # Start the Bot
+    updater.start_polling()
+
+    updater.idle()
+    print('Finish')
+
+option = ""
+
+if __name__ == '__main__':
+    main()
